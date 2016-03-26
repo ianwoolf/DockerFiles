3a8e2e7c64c6b11eeb6cab0306bf8300ca84509b
ianwoolf <btw515wolf2@gmail.com>
2016-03-27 00:05:03 +0800

    del file

--- etcd/run.sh
+++ etcd/run.sh
@@ -3,5 +3,5 @@ echo "root" | passwd –stdin root
 echo "root:root"|chpasswd
 sed -i '/^PermitRootLogin/s/without-password/yes/g' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
-/bin/etcd -data-dir=/data/etcd -name=`hostname`
+/bin/etcd -data-dir=/data/etcd -name=`hostname` &
 /bin/bash
