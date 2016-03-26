1dfc4b96d8617dc9765c7c376d5c0c931f8f72f3
ianwoolf <btw515wolf2@gmail.com>
2016-03-27 01:38:52 +0800

    fix sudo bug update .gitignore

--- hadoop/start-container.sh
+++ hadoop/start-container.sh
@@ -11,9 +11,9 @@ fi
 	
 
 # delete old master container and start new master container
-sudo docker rm -f master &> /dev/null
+docker rm -f master &> /dev/null
 echo "start master container..."
-sudo docker run -d -t --dns 127.0.0.1 -P --name master -h master.zzz -w /root hadoop:master &> /dev/null
+docker run -d -t --dns 127.0.0.1 -P --name master -h master.zzz -w /root hadoop:master &> /dev/null
 
 # get the IP address of master container
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" master)
@@ -23,12 +23,12 @@ i=1
 while [ $i -lt $N ]
 do
 	echo $i,$N
-	sudo docker rm -f slave${i}.zzz &> /dev/null
+	docker rm -f slave${i}.zzz &> /dev/null
 	echo "start slave$i container..."
-	sudo docker run -d -t --dns 127.0.0.1 -P --name slave${i} -h slave${i}.zzz -e JOIN_IP=${FIRST_IP} hadoop:slave &> /dev/null
+	docker run -d -t --dns 127.0.0.1 -P --name slave${i} -h slave${i}.zzz -e JOIN_IP=${FIRST_IP} hadoop:slave &> /dev/null
 	i=$(( $i + 1 ))
 done 
 
 
 # create a new Bash session in the master container
-sudo docker exec -it master bash
+docker exec -it master bash
